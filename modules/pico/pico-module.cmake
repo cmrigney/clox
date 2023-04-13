69b8b34c25d02387dc5448f34b2304dff826ff2a
Cody Rigney <codyrigney92@gmail.com>
2023-04-12 21:27:14 -0400

    Analog reading implementation.

--- modules/pico/pico-module.cmake
+++ modules/pico/pico-module.cmake
@@ -8,15 +8,15 @@ pico_sdk_init()
 
 add_compile_definitions(PICO_MODULE)
 
+target_link_libraries(clox pico_stdlib hardware_adc)
+
 if(DEFINED ENV{USE_PICO_W})
   message("Building for Pico W")
   target_include_directories(clox PRIVATE
     ${CMAKE_CURRENT_LIST_DIR} # for lwipopts.h
   )
-  target_link_libraries(clox pico_cyw43_arch_lwip_poll pico_stdlib)
+  target_link_libraries(clox pico_cyw43_arch_lwip_poll)
   add_compile_definitions(USE_PICO_W)
-else()
-  target_link_libraries(clox pico_stdlib)
 endif()
 
 # enable usb output, disable uart output
